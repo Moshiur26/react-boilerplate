9c86b2396035c98cdc6fbc5637e991a20b909987
moshiur26 <moshiurrahmanb11@gmail.com>
2022-03-04 16:07:23 +0600

    add material-ui style into home page

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,7 +1,40 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Typography from '@material-ui/core/Typography'
+import homeImg from './../assets/images/home_1.jpg'
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    maxWidth: 600,
+    margin: 'auto',
+    marginTop: theme.spacing(5),
+    marginBottom: theme.spacing(5)
+  },
+  title: {
+    padding:`${theme.spacing(3)}px ${theme.spacing(2.5)}px ${theme.spacing(2)}px`,
+    color: theme.palette.openTitle
+  },
+  media: {
+    minHeight: 400
+  }
+}))
 
 export default function Home(){
+  const classes = useStyles()
     return (
-      <h5>Hello This Is Moshiur</h5>  
-      )
+        <Card className={classes.card}>
+          <Typography variant="h6" className={classes.title}>
+            Home Page
+          </Typography>
+          <CardMedia className={classes.media} image={homeImg} />
+          <CardContent>
+            <Typography variant="body1" component="p">
+              Welcome to the MERN Skeleton home page.
+            </Typography>
+          </CardContent>
+        </Card>
+    )
 }
