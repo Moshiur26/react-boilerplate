95c5fe4cd064b4463b6c2455fea6f58608897a92
moshiur26 <moshiurrahmanb11@gmail.com>
2022-04-01 01:28:51 +0600

    add menu bar into route

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import {Route, Routes} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Home from './views/core/Home'
 import Signup from './views/user/Signup'
 import Signin from './views/user/Signin'
+import Menu from './views/core/Menu'
+
 const MainRouter = () => {
-    return (<div>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="signup" element={<Signup />} />
-        <Route path="signin" element={<Signin />} />
-      </Routes>
+    return (
+    <div>
+        <Menu />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="signin" element={<Signin />} />
+        </Routes>
     </div>)
 }
 
